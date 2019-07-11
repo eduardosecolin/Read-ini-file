be99f0bec90720f36291f448a6ed8758eaa88284
Eduardo Secolin <dusecolin@yahoo.com.br>
2019-07-11 18:33:56 -0300

    Adicionar arquivos de projeto.

---
+++ LerIni/IniFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace LerIni
+{
+    public class IniFile
+    {
+        private readonly string path;
+        private int capacity = 512;
+
+        [DllImport("Kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string defaultValue, StringBuilder value, int size, string filePath);
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetPrivateProfileString(string section, string key, string defaultValue, [In, Out] char[] value, int size, string filePath);
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetPrivateProfileString(string section, IntPtr keyValue, int size, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        public IniFile(string _path)
+        {
+            path = _path;
+        }
+
+        public string ReadValue(string section, string key, string defaultValue = "")
+        {
+            var value = new StringBuilder(capacity);
+            GetPrivateProfileString(section, key, defaultValue, value, value.Capacity, path);
+            return value.ToString();
+        }
+    }
+}
---
+++ LerIni/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LerIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Your Path!";
+            IniFile iniFile = new IniFile(path);
+
+            string keyValue = iniFile.ReadValue("[SECTION]", "KEY");
+
+            Console.WriteLine($"Value: {keyValue}");
+        }
+    }
+}
